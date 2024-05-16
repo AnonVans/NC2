30f30c5b20246866a57f5677050b9ff5526c4717
Vans <stevanstc2018@gmail.com>
2024-05-17 02:18:54 +0700

    Initial Commit

---
+++ NC2/NC2App.swift
@@ -0,0 +1,17 @@
+//
+//  NC2App.swift
+//  NC2
+//
+//  Created by Stevans Calvin Candra on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct NC2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
